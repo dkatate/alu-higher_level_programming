f118d2821d0f758927d1774c2fcf15e20832f229
dkatate <dkatate@alustudent.com>
2025-06-21 10:44:09 -0700

    7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[7:]
-middle_word = word[1:8]
+word_first_3 = word[:3]          # First 3 chars
+word_last_2 = word[-2:]         # Last 2 chars
+middle_word = word[1:-1]        # Excludes first and last
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
