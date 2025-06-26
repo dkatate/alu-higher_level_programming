8e53cd162ea6cc3d1b443929683a4036b0af341e
dkatate <dkatate@alustudent.com>
2025-06-26 05:32:06 -0700

    2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" function to replace"""
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    """Replace an element in a list at a specific position"""
+    # Check if the index is valid
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+
+    # If valid, replace the element
+    my_list[idx] = element
+    return my_list
