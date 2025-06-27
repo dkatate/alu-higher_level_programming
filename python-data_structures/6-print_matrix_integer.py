80e132818ceaf7a3286894a157221b1d0744ba2e
dkatate <dkatate@alustudent.com>
2025-06-26 22:51:21 -0700

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
+"""function for matrix"""
+
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            print("{:d}".format(row[i]), end=" " if i < len(row) - 1 else "")
+        for i, num in enumerate(row):
+            if i != len(row) - 1:
+                print("{:d}".format(num), end=" ")
+            else:
+                print("{:d}".format(num), end="")
         print()
