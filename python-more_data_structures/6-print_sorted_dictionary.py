34f1b574659e7937eeef0275d561e32dd78c842b
dkatate <dkatate@alustudent.com>
2025-06-30 01:40:26 -0700

    6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """prints dictionary"""
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
