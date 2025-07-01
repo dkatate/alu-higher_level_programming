faa4d8aaf49a5d00c3eb2e110f0a6cb8c2948474
dkatate <dkatate@alustudent.com>
2025-07-01 14:02:59 -0700

    2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x integers in a list."""
+    count = 0
+    try:
+        for i in my_list[:x]:
+            if isinstance(i, int):
+                print("{:d}".format(i), end="")
+                count += 1
+    except Exception:
+        pass
+    print()
+    return count
