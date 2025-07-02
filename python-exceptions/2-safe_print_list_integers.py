73a3a50e422f3ec0b2004a0204020bbb7baeca8d
dkatate <dkatate@alustudent.com>
2025-07-01 22:06:39 -0700

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x integers in a list."""
+    """print the first x"""
     count = 0
-    try:
-        for i in range(x):
-            if isinstance(i, int):
-                print("{:d}".format(i), end="")
-                count += 1
-    except Exception:
-        pass
-    print()
-    return count
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            raise
+        print()
+        return count
