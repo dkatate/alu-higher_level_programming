dcfe95d580ad40d5a409f296d8b0b1bf0383e1ab
dkatate <dkatate@alustudent.com>
2025-07-06 01:13:32 -0700

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module defining a square class
+"""
+
+
+class Square:
+    """Define a square with a priivate size"""
+    def __init__(self, size):
+        self.__size = size
