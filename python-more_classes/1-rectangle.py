2d9d96c97c4e978abb68315b6ef8f69ac9e1d687
dkatate <dkatate@alustudent.com>
2025-07-09 22:37:41 -0700

    1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module for class Rectangle."""
+
+class Rectangle:
+    """Rectangle with private width and height attributes."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width  # calls setter, ensures validation
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with type/size checks."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height with type/size checks."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
