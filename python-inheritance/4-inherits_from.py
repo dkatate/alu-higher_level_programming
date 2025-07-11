a6bcd34010923d5a197365aff6ae66b320dcc901
dkatate <dkatate@alustudent.com>
2025-07-11 04:11:34 -0700

    4-inherits_from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""inheriting directly"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class
+    that inherited. otherwise False
+    """
+    return issu
