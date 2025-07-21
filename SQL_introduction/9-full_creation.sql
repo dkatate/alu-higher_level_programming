29a9b47665a6c96f66da8e89854b90029783136c
dkatate <dkatate@alustudent.com>
2025-07-21 16:03:24 -0700

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,9 @@
--- create table and update
-CREATE TABLE IF NOT EXISTS second_table(                                               
-id, INT
-name, VARCHAR(256)
-score, INT
-);
+-- Query that creates a table second_table in the database hbtn_0c_0 and fills it with some data.
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
-VALUES((1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8));
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
